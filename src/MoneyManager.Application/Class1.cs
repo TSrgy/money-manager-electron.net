d60025cc7f97c2eed16d4ec48c8461f57773792a
TSrgy <20205309+TSrgy@users.noreply.github.com>
2020-11-21 08:47:52 +0500

    Add projects

---
+++ src/MoneyManager.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MoneyManager.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ src/MoneyManager.Domain/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace MoneyManager.Domain
+{
+    public class Class1
+    {
+    }
+}
