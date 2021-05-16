8015df00891c76581adc02784ef599c6cbc5078b
TSrgy <20205309+TSrgy@users.noreply.github.com>
2021-05-16 16:15:01 +0500

    Add test

--- tests/Application.IntegrationTests/Accounts/Queries/GetAccountsQueryTests.cs
+++ tests/Application.IntegrationTests/Accounts/Queries/GetAccountsQueryTests.cs
@@ -25,15 +25,9 @@ namespace Application.IntegrationTests.Accounts.Queries
             var name1 = "test1";
             var name2 = "test2";
 
-            await Fixture.AddAsync(new Account()
-            {
-                Name = name1
-            });
+            await Fixture.AddAsync(new Account(name1, AccountType.Checking, 0, new Currency("test", "TST")));
 
-            await Fixture.AddAsync(new Account()
-            {
-                Name = name2
-            });
+            await Fixture.AddAsync(new Account(name2, AccountType.Checking, 0, new Currency("test", "TST")));
 
             var accountsVm = await Fixture.SendAsync(query);
 
